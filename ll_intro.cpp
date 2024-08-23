41f8df69f755fcad21a62d6b83ca3e763b363b51
jatinchaudhary <jatinchaudhary41@gmail.com>
2024-08-23 14:10:29 +0530

    ll continue

--- ll_intro.cpp
+++ ll_intro.cpp
@@ -96,14 +96,103 @@ Node* reverse_ll(Node* head){
 
 }
 
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+
+        ListNode* start = head;
+        int length = 0;
+
+        while(start!=NULL){
+            start=start->next;
+            length++;
+        }
+
+        int targetNode = length - n;
+
+        if(n==length){
+            head = head->next;
+            return head;
+        }
+
+        ListNode* temp = nullptr;
+        start = head;
+
+        while(targetNode!=1){
+            start = start->next;
+            targetNode--;
+        }
+
+        temp = start->next->next;
+        start->next = temp;
+
+        return head;
+
+        
+    }
+
+
+
+Node* reorder_ll(Node* head){
+
+Node* mid = head;
+Node* end = head;
+
+while(end!=NULL && end->next!=NULL){
+    mid = mid->next;
+    end = end->next->next;
+}
+
+cout<<mid->data;
+
+Node* prev = nullptr;
+Node* curr = mid->next;
+Node* next = mid->next;
+
+while(curr!=nullptr){
+    next = curr->next;
+    curr->next = prev;
+    prev = curr;
+    curr = next;
+}
+
+mid->next = prev;
+
+traverse_ll(head);
+
+Node* start = head;
+Node* temp = head;
+
+Node* midPrev = mid;
+
+mid = mid->next;
+while(mid!=nullptr){
+
+temp = start->next;
+start->next = mid;
+midPrev->next = mid->next;
+mid->next = temp;
+
+mid = midPrev->next;
+start = start->next->next;
+}
+
+
+cout<<endl;
+traverse_ll(head);
+
+
+return head;
+
+}
+
 
 int main(){
 
-Node* head = create_ll({3,4,5,6,7,8});
-traverse_ll(head);
-head = delete_ll_element(head,0);
-traverse_ll(head);
-head = reverse_ll(head);
-traverse_ll(head);
+Node* head = create_ll({1,2,3,4,5,6});
+// traverse_ll(head);
+// head = delete_ll_element(head,0);
+// traverse_ll(head);
+// head = reverse_ll(head);
+reorder_ll(head);
+//traverse_ll(head);
 return 0;
 }
